8f3e828afeb9eedfec0ad83e6d40b9d05ef26630
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-15 15:27:23 -0300

    -- NavBar Mobile --

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom"
 import { Divide as Hamburger } from 'hamburger-react'
 import { useState } from "react"
 import "./NavBar.css"
+
 const NavBar = () => {
     const [isOpen, setOpen] = useState(false)
     return (
         <>
-            <div className="nav-container">
+            <div className={`nav-container ${isOpen && "background-navbar-active"}`}>
                 <img className="logo" src={LogoIcon} alt="Logo" />
 
                 <Hamburger color="#fff" toggled={isOpen} toggle={setOpen} />
@@ -16,10 +17,10 @@ const NavBar = () => {
             {isOpen &&
                 <nav className="navbar">
                     <ul className="navbar-ul">
-                        <li className="navbar-li"><Link className="navbar-a">Home</Link></li>
-                        <li className="navbar-li"><Link className="navbar-a">Destination</Link></li>
-                        <li className="navbar-li"><Link className="navbar-a">Crew</Link></li>
-                        <li className="navbar-li"><Link className="navbar-a">Technology</Link></li>
+                        <li className="navbar-li"><Link className="navbar-a">00. <span>Home</span> </Link></li>
+                        <li className="navbar-li"><Link className="navbar-a">01. <span>Destination</span></Link></li>
+                        <li className="navbar-li"><Link className="navbar-a">02. <span>Crew</span></Link></li>
+                        <li className="navbar-li"><Link className="navbar-a">03. <span>Technology</span></Link></li>
                     </ul>
                 </nav>
             }
