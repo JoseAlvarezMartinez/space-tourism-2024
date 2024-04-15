6fe69d7a9d344911c10f1bd0c580b1c5f6d561d1
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-15 13:35:37 -0300

    -- Background Images --

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
-import {crew} from "./db/data"
 function App() {
-console.log(crew)
+
   return (
     <>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
