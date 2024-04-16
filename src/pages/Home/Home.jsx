cd97b3ca4c8b87853a756dda43e8505b6b6c4706
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-16 11:56:33 -0300

    -- Desktop Home --

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,15 +5,16 @@ const Home = () => {
     return (
         <header className="header-home">
             <NavBar />
-            <div className="header-home-container">
-                <h2 className="header-h2">So, you want to travel to <span>space</span></h2>
-                <p className="header-p">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
-            </div>
+            <div className="desktop-responsive-container">
+                <section className="header-home-container">
+                    <h2 className="header-h2">So, you want to travel to <span>space</span></h2>
+                    <p className="header-p">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                </section>
 
-            <div className="explore-circle">
-                <Link className="explore-circle-a">Explore</Link>
+                <div className="explore-circle">
+                    <Link className="explore-circle-a">Explore</Link>
+                </div>
             </div>
-
         </header>
     )
 }
