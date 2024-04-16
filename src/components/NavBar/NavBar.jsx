a7229c9cee0044ab75f68de78a519e151600ce16
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-16 10:15:22 -0300

    -- useLocation to navbar --

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import LogoIcon from "../../assets/logo.svg"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import { Divide as Hamburger } from 'hamburger-react'
 import { useEffect } from "react"
 import useBodyFlow from "../../hooks/useBodyFlow"
@@ -8,6 +8,7 @@ import "./NavBar.css"
 const NavBar = () => {
 
     const { isOpen, handleBodyFlow, setOpen } = useBodyFlow()
+    const { pathname } = useLocation()
 
     useEffect(() => {
         handleBodyFlow()
@@ -29,10 +30,10 @@ const NavBar = () => {
 
                 <nav className={`navbar ${isOpen ? "navbar-active" : "navbar-inactive"}`}>
                     <ul className="navbar-ul">
-                        <li className="navbar-li"><Link to={"/"} className="navbar-a"> <span>00.</span>Home</Link></li>
-                        <li className="navbar-li"><Link to={"/destination"} className="navbar-a"><span>01.</span>Destination</Link></li>
-                        <li className="navbar-li"><Link to={"/crew"} className="navbar-a"><span>02.</span>Crew</Link></li>
-                        <li className="navbar-li"><Link to={"/technology"} className="navbar-a"> <span>03</span>Technology</Link></li>
+                        <li className="navbar-li"><Link to={"/"} className={`navbar-a ${pathname === "/" && "active-navbar-link"}`}> <span>00.</span>Home</Link></li>
+                        <li className="navbar-li"><Link to={"/destination"} className={`navbar-a ${pathname === "/destination" && "active-navbar-link"}`}><span>01.</span>Destination</Link></li>
+                        <li className="navbar-li"><Link to={"/crew"} className={`navbar-a ${pathname === "/crew" && "active-navbar-link"}`}><span>02.</span>Crew</Link></li>
+                        <li className="navbar-li"><Link to={"/technology"} className={`navbar-a ${pathname === "/technology" && "active-navbar-link"}`}> <span>03</span>Technology</Link></li>
                     </ul>
                 </nav>
             </div>
