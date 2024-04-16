1732c835d8f2f26b3602725ba5b801150d89354c
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-04-16 12:45:16 -0300

    -- Destination Desktop --

--- src/pages/Destination/Destination.jsx
+++ src/pages/Destination/Destination.jsx
@@ -13,29 +13,34 @@ const Destination = () => {
             <NavBar />
 
             <p className="destination-p">01. <span>pick your destination</span></p>
-            <img className="destination-image" src={image} alt={`${name} Image`} />
-            <div className="destination-planet-link">
-                {planets.map(planet =>
-                    <Link
-                        key={planet.id}
-                        to={`/destination/${planet.id}`}
-                        onClick={() => setActualPlanet(planet)}
-                        className={`planet-link ${actualPlanet.id === planet.id && "active-link"} `}>{planet.name}</Link>
-                )}
+            <div className="destination-flex-desktop">
+                <img className="destination-image" src={image} alt={`${name} Image`} />
 
-            </div>
-            <h2 className="destination-planet-name">{name}</h2>
-            <p className="destination-planet-description">{description}</p>
+                <div className="destination-desktop-container">
+                    <div className="destination-planet-link">
+                        {planets.map(planet =>
+                            <Link
+                                key={planet.id}
+                                to={`/destination/${planet.id}`}
+                                onClick={() => setActualPlanet(planet)}
+                                className={`planet-link ${actualPlanet.id === planet.id && "active-link"} `}>{planet.name}</Link>
+                        )}
 
-            <div className="destination-planet-data">
-                <section>
-                    <h3 className="destination-planet-distance-h3">avg. distance</h3>
-                    <p className="destination-planet-distance-p">{distance}</p>
-                </section>
-                <section>
-                    <h3 className="destination-planet-distance-h3">est. travel time</h3>
-                    <p className="destination-planet-distance-p">{travelTime}</p>
-                </section>
+                    </div>
+                    <h2 className="destination-planet-name">{name}</h2>
+                    <p className="destination-planet-description">{description}</p>
+
+                    <div className="destination-planet-data">
+                        <section>
+                            <h3 className="destination-planet-distance-h3">avg. distance</h3>
+                            <p className="destination-planet-distance-p">{distance}</p>
+                        </section>
+                        <section>
+                            <h3 className="destination-planet-distance-h3">est. travel time</h3>
+                            <p className="destination-planet-distance-p">{travelTime}</p>
+                        </section>
+                    </div>
+                </div>
             </div>
         </main >
     )
